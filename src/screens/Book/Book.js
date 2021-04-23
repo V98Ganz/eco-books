0ab5436fb68ce86699943bbcf598cffc3155f043
Eszter Tarjanyi <eszter.tarjanyi94@gmail.com>
2021-04-23 16:38:25 +0100

    add carousel with image and text

--- src/screens/Book/Book.js
+++ src/screens/Book/Book.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { Image, Text, TouchableHighlight, View } from 'react-native';
+import { Dimensions, Image, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+
+export const SLIDER_WIDTH = Dimensions.get('window').width + 80
+export const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.7)
 
 export default class Book extends Component {
   state = {
@@ -15,23 +18,60 @@ export default class Book extends Component {
       }
     })
   }
-
+  
   render() {
-    const { showingPicture } = this.state;
+    
     return (
-      showingPicture ? 
+      <View style={styles.container} key={this.props.index} >
+      {this.state.showingPicture} ?
       <TouchableHighlight onPress={() => this.changeState()} >
-        <Image source={{uri: this.props.bookImage}} style={{width: 400, height: 550}}  />
+        <Image source={{uri: this.props.item.bookImage}} style={styles.image}  />
       </TouchableHighlight>
       : 
       <TouchableHighlight onPress={() => this.changeState()}>
         <View>
-          <Text >{this.props.bookAuthor}</Text>
-          <Text>{this.props.bookTitle}</Text>
-          <Text>{this.props.bookDescription}</Text>
+          <Text style={styles.header}>{this.props.item.bookAuthor}</Text>
+          <Text style={styles.header}>{this.props.item.bookTitle}</Text>
+          <Text style={style.body}>{this.props.item.bookDescription}</Text>
         </View>
       </TouchableHighlight>
+      </View>
 
     )
   }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      backgroundColor: 'white',
+      borderRadius: 8,
+      width: ITEM_WIDTH,
+      paddingBottom: 40,
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 3,
+      },
+      shadowOpacity: 0.29,
+      shadowRadius: 4.65,
+      elevation: 7,
+    },
+    image: {
+      width: ITEM_WIDTH,
+      height: 300,
+    },
+    header: {
+      color: "#222",
+      fontSize: 28,
+      fontWeight: "bold",
+      paddingLeft: 20,
+      paddingTop: 20
+    },
+    body: {
+      color: "#222",
+      fontSize: 18,
+      paddingLeft: 20,
+      paddingLeft: 20,
+      paddingRight: 20
+    }
+  })
--- src/screens/BookShop/BookShop.js
+++ src/screens/BookShop/BookShop.js
@@ -1,52 +1,91 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { Image, Text, View } from 'react-native';
+import Carousel from 'react-native-snap-carousel';
 import { firebase } from '../../firebase/config';
-import { default as Book } from '../Book/Book';
+import styles from '../LoginScreen/styles';
 
  export default class BookShop extends Component {
+
   state = {
-    books: []
+    books: [],
+    showingPicture: true
   }
 
-  componentDidMount = () => {
-    firebase
-        .firestore()
-        .collection("users")
-        .get()
-        .then((querySnapshot) => {
-          querySnapshot.forEach((doc) => {
-            let usersBooks = [];
-            let userId = doc.id;
-            firebase.firestore()
-            .collection("users")
-            .doc(userId)
-            .collection("books")
-            .get()
-            .then((querySnapshot) => {
+  changeState = () => {
+    this.setState((currentState) => {
+      if (currentState.showingPicture) {
+        return { showingPicture: false }
+      } else {
+        return { showingPicture: true }
+      }
+    })
+  }
+
+  _renderItem = ({ item, index }) => {
+    
+    return (
+      <View style={styles.slide}>
+        <Image source={{uri: item.bookImage}} style={{width: 200, height: 200}}  />
+        <Text style={styles.title}>{item.bookAuthor}</Text>
+        <Text style={styles.title}>{item.bookTitle}</Text>
+        <Text stlye={styles.body}>{ item.bookDescription }</Text>
+      </View>
+    )
+    }
+    
+    componentDidMount = () => {
+      firebase
+      .firestore()
+      .collection("users")
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          let usersBooks = [];
+          let userId = doc.id;
+          firebase.firestore()
+          .collection("users")
+          .doc(userId)
+          .collection("books")
+          .get()
+          .then((querySnapshot) => {
             querySnapshot.forEach((doc) => {
               const bookData = doc.data()
               usersBooks.push(bookData)
-              })
-              this.setState((currentState) => {
-                const updatedBooks = [...currentState.books, ...usersBooks];
-                return { books: updatedBooks};
-              })
+            })
+            this.setState((currentState) => {
+              const updatedBooks = [...currentState.books, ...usersBooks];
+              return { books: updatedBooks};
             })
           })
         })
-  }
+      })
+    }
     
     render() {
-      const { bookImage, bookDescription, bookAuthor, bookTitle } = this.state.books
-      console.log(this.state)
-      return this.state.books.map((book) => {
-        return (
-            <View>
-              <Book bookDescription={book.bookDescription} bookImage={book.bookImage} bookAuthor={book.bookAuthor} bookTitle={book.bookTitle}/>
-            </View>
-        )
-      })
-    };
+      return (
+        <View>
+        <Carousel
+        layout={"default"}
+        ref={ref => this.carousel = ref} 
+        data={this.state.books}
+        renderItem={this._renderItem}
+        sliderWidth={300}
+        itemWidth={300}
+        />
+      </View>
+    )
+    
   }
+}
 
-  
+
+//   const { bookImage, bookDescription, bookAuthor, bookTitle } = this.state.books
+//   console.log(this.state)
+//   return this.state.books.map((book) => {
+  //     return (
+    //         <View>
+    //           <Book bookDescription={book.bookDescription} bookImage={book.bookImage} bookAuthor={book.bookAuthor} bookTitle={book.bookTitle}/>
+    //         </View>
+    //     )
+    //   })
+    // };
