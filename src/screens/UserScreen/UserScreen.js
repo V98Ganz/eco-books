9e69c5d509b418443832446c6d826c7972f54285
Daniel Saleh <danielsalehguitar@gmail.com>
2021-04-27 23:19:15 +0100

    add initial styling to userProfile

--- src/screens/UserScreen/UserScreen.js
+++ src/screens/UserScreen/UserScreen.js
@@ -3,13 +3,21 @@ import { Text, View } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import AddBookFrom from "./AddBookForm";
 import MyBooksCarousel from "./MyBooksCarousel";
+import styles from "./styles";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import {
+  faBook,
+  faQuestionCircle,
+  faPlusCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import { CardStyleInterpolators } from "@react-navigation/stack";
 
 export default class UserScreen extends Component {
   state = {
     showingMyBooks: false,
     showingEcobookInstructions: false,
     showingAddNewBook: false,
-    addedNewBook: false
+    addedNewBook: false,
   };
 
   viewMyBooks = () => {
@@ -54,35 +62,56 @@ export default class UserScreen extends Component {
         return { addedNewBook: true };
       }
     });
-  }
+  };
 
   render() {
     return (
-      <View>
-      <KeyboardAwareScrollView>
-        <Text>Welcome back {this.props.user.fullName}!</Text>
-        <Text onPress={this.viewEcobookInstructions}>
-          How does EcoBooks work?
-        </Text>
-        {this.state.showingEcobookInstructions ? (
-          <Text>
-            Ecobooks is an app where you can buy and sell books. Instead of
-            using money, you can make Bookcoins by walkings. Walking 100 steps
-            can earn you one bookCoin. With this money you are able to then buy
-            and sell your loved and used books to others.
+      <View style={styles.content_container}>
+        <KeyboardAwareScrollView>
+          <Text>Welcome back {this.props.user.fullName}!</Text>
+          <View style={styles.section_icons}>
+            <FontAwesomeIcon icon={faQuestionCircle} size={70} />
+          </View>
+          <Text
+            style={styles.section_headers}
+            onPress={this.viewEcobookInstructions}
+          >
+            How does EcoBooks work?
           </Text>
-        ) : null}
-        <Text onPress={this.viewMyBooks}>Your Books</Text>
-        {this.state.showingMyBooks ? (
-          <MyBooksCarousel user={this.props.user} addedBook={this.state.addedNewBook}/>
-        ) : null}
-        <Text onPress={this.viewAddNewBooks}>Add a new Book!</Text>
-        {this.state.showingAddNewBook ? (
-          <AddBookFrom user={this.props.user} updateAddedNewBook={this.updateAddedNewBook}/>
-        ) : null}
-      </KeyboardAwareScrollView>
+          {this.state.showingEcobookInstructions ? (
+            <Text style={styles.about_ecobooks}>
+              Ecobooks is an app where you can buy and sell books. Instead of
+              using money, you can make Bookcoins by walkings. Walking 100 steps
+              can earn you one bookCoin. With this money you are able to then
+              buy and sell your loved and used books to others.
+            </Text>
+          ) : null}
+          <View style={styles.section_icons}>
+            <FontAwesomeIcon icon={faBook} size={70} />
+          </View>
+          <Text style={styles.section_headers} onPress={this.viewMyBooks}>
+            Your Books
+          </Text>
+          {this.state.showingMyBooks ? (
+            <MyBooksCarousel
+              user={this.props.user}
+              addedBook={this.state.addedNewBook}
+            />
+          ) : null}
+          <View style={styles.section_icons}>
+            <FontAwesomeIcon icon={faPlusCircle} size={70} />
+          </View>
+          <Text style={styles.section_headers} onPress={this.viewAddNewBooks}>
+            Add a new Book!
+          </Text>
+          {this.state.showingAddNewBook ? (
+            <AddBookFrom
+              user={this.props.user}
+              updateAddedNewBook={this.updateAddedNewBook}
+            />
+          ) : null}
+        </KeyboardAwareScrollView>
       </View>
-      
     );
   }
 }
---
+++ src/screens/UserScreen/styles.js
@@ -0,0 +1,33 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  // universal styles
+  section_headers: {
+    fontSize: 25,
+    color: "#3f3f3f",
+    padding: 15,
+    textAlign: "center",
+  },
+
+  // how ecobooks works section
+  content_container: {
+    borderWidth: 3,
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+  },
+
+  about_ecobooks: {
+    fontSize: 20,
+    color: "blue",
+    padding: 20,
+    lineHeight: 35,
+    textAlign: "center",
+  },
+
+  section_icons: {
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 20,
+  },
+});
