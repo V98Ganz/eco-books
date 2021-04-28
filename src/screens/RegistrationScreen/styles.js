646897a42af427b5ab49f297f78c433453b73b9b
Alex Gada <alexgada01@gmail.com>
2021-04-28 10:28:30 +0100

    added registration screen styling

--- src/screens/RegistrationScreen/styles.js
+++ src/screens/RegistrationScreen/styles.js
@@ -1,58 +1,71 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center'
-    },
-    title: {
-
-    },
-    logo: {
-        flex: 1,
-        height: 120,
-        width: 90,
-        alignSelf: "center",
-        margin: 30
-    },
-    input: {
-        height: 48,
-        borderRadius: 5,
-        overflow: 'hidden',
-        backgroundColor: 'white',
-        marginTop: 10,
-        marginBottom: 10,
-        marginLeft: 30,
-        marginRight: 30,
-        paddingLeft: 16
-    },
-    button: {
-        backgroundColor: '#788eec',
-        marginLeft: 30,
-        marginRight: 30,
-        marginTop: 20,
-        height: 48,
-        borderRadius: 5,
-        alignItems: "center",
-        justifyContent: 'center'
-    },
-    buttonTitle: {
-        color: 'white',
-        fontSize: 16,
-        fontWeight: "bold"
-    },
-    footerView: {
-        flex: 1,
-        alignItems: "center",
-        marginTop: 20
-    },
-    footerText: {
-        fontSize: 16,
-        color: '#2e2e2d'
-    },
-    footerLink: {
-        color: "#788eec",
-        fontWeight: "bold",
-        fontSize: 16
-    }
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  title: {},
+  logo: {
+    flex: 1,
+    height: 200,
+    width: 275,
+    alignSelf: "center",
+    margin: 30,
+    paddingTop: 80,
+    paddingBottom: 80,
+  },
+  input: {
+    height: 48,
+    borderRadius: 5,
+    overflow: "hidden",
+    backgroundColor: "white",
+    borderWidth: 3,
+    borderColor: "#d3d3d3",
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 30,
+    marginRight: 30,
+    paddingLeft: 16,
+  },
+  button: {
+    backgroundColor: "#389f30",
+    marginLeft: 30,
+    marginRight: 30,
+    marginTop: 20,
+    height: 48,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonTitle: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  insertBookText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    alignItems: "center",
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 120,
+    color: "#389f30",
+  },
+  footerView: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 15,
+    marginBottom: 80,
+  },
+  footerText: {
+    fontSize: 16,
+    color: "#2e2e2d",
+  },
+  footerLink: {
+    color: "#389f30",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+});
