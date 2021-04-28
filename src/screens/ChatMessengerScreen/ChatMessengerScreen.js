fac4e56706d1a3d64a232e4ca3f4a84a6aa4cba7
Daniel Saleh <danielsalehguitar@gmail.com>
2021-04-28 10:46:48 +0100

    add basic styling to messenger inbox

--- src/screens/ChatMessengerScreen/ChatMessengerScreen.js
+++ src/screens/ChatMessengerScreen/ChatMessengerScreen.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { Button, View, Text, ScrollView, TouchableOpacity } from "react-native";
+import {
+  Button,
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 import { firebase } from "../../firebase/config";
 import { default as ConversationScreen } from "../ConversationScreen/ConversationScreen";
 import styles from "./styles";
@@ -124,6 +131,10 @@ export default class ChatMessengerScreen extends React.Component {
             {roomzzEntries.map((array) => {
               return (
                 <View style={styles.roomLink} key={array[0]}>
+                  <Image
+                    style={styles.eco_logo}
+                    source={require("../../img/ecobooks.png")}
+                  ></Image>
                   <Text style={styles.chat_link_text}>{array[1].to}</Text>
                   <TouchableOpacity
                     style={styles.chat_button}
@@ -135,7 +146,7 @@ export default class ChatMessengerScreen extends React.Component {
                       )
                     }
                   >
-                    <Text>Enter Chat</Text>
+                    <Text style={styles.chat_button_text}>Enter Chat</Text>
                   </TouchableOpacity>
                 </View>
               );
