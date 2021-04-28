b79625d3857bb32b1c88d86b0967d80016f6788e
Daniel Saleh <danielsalehguitar@gmail.com>
2021-04-28 11:45:33 +0100

    add initial styling to coin registration page

--- src/screens/RegistrationScreen/RegistrationCoinInfo.js
+++ src/screens/RegistrationScreen/RegistrationCoinInfo.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
-import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+import {
+  Image,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  ScrollView,
+} from "react-native";
 import { firebase } from "../../firebase/config";
+import styles from "./styles";
 
 export default class RegistrationCoinInfo extends Component {
   state = {
@@ -57,33 +65,40 @@ export default class RegistrationCoinInfo extends Component {
     }
 
     return (
-      <View>
-        <Text>Welcome to EcoBooks!</Text>
-        <Text>
-          EcoBooks is a book trading app where we encourage you to read, walk
-          and recycle! Each book in our BookShop is worth BookCoins, which you
-          can earn by walking.
-        </Text>
-        <Text>
-          You have added {this.state.myBooks.bookTitle} for sale, please add how
-          many BookCoins you would like to sell your book for. Book value must
-          be between 1 to 5 BookCoins.
-        </Text>
-        <Image
-          source={{ uri: this.state.myBooks.bookImage }}
-          style={{ width: 295, height: 450, resizeMode: "contain" }}
-        />
-        <TextInput
-          placeholderTextColor="#aaaaaa"
-          placeholder="Input BookCoin price"
-          onChangeText={this.handleBookCoin}
-          underlineColorAndroid="transparent"
-          autoCapitalize="none"
-        />
-        <TouchableOpacity onPress={this.nextPage}>
-          <Text>Get started!</Text>
-        </TouchableOpacity>
-      </View>
+      <ScrollView>
+        <View style={styles.content_container}>
+          <Image
+            style={styles.eco_logo}
+            source={require("../../img/ecobooks.png")}
+          ></Image>
+          <Text style={styles.welcome_text}>Welcome to EcoBooks!</Text>
+          <Text style={styles.eco_about}>
+            EcoBooks is a book trading app where we encourage you to read, walk
+            and recycle! Each book in our BookShop is worth BookCoins, which you
+            can earn by walking.
+          </Text>
+          <View style={styles.line_break}></View>
+          <Text style={styles.eco_about}>
+            You have added {this.state.myBooks.bookTitle} for sale, please add
+            how many BookCoins you would like to sell your book for. Book value
+            must be between 1 to 5 BookCoins.
+          </Text>
+          <Image
+            source={{ uri: this.state.myBooks.bookImage }}
+            style={{ width: 295, height: 450, resizeMode: "contain" }}
+          />
+          <TextInput
+            placeholderTextColor="#aaaaaa"
+            placeholder="Input BookCoin price"
+            onChangeText={this.handleBookCoin}
+            underlineColorAndroid="transparent"
+            autoCapitalize="none"
+          />
+          <TouchableOpacity onPress={this.nextPage}>
+            <Text>Get started!</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     );
   }
 }
--- src/screens/RegistrationScreen/styles.js
+++ src/screens/RegistrationScreen/styles.js
@@ -1,58 +1,90 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center'
-    },
-    title: {
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  title: {},
+  logo: {
+    flex: 1,
+    height: 120,
+    width: 90,
+    alignSelf: "center",
+    margin: 30,
+  },
+  input: {
+    height: 48,
+    borderRadius: 5,
+    overflow: "hidden",
+    backgroundColor: "white",
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 30,
+    marginRight: 30,
+    paddingLeft: 16,
+  },
+  button: {
+    backgroundColor: "#788eec",
+    marginLeft: 30,
+    marginRight: 30,
+    marginTop: 20,
+    height: 48,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonTitle: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  footerView: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 20,
+  },
+  footerText: {
+    fontSize: 16,
+    color: "#2e2e2d",
+  },
+  footerLink: {
+    color: "#788eec",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
 
-    },
-    logo: {
-        flex: 1,
-        height: 120,
-        width: 90,
-        alignSelf: "center",
-        margin: 30
-    },
-    input: {
-        height: 48,
-        borderRadius: 5,
-        overflow: 'hidden',
-        backgroundColor: 'white',
-        marginTop: 10,
-        marginBottom: 10,
-        marginLeft: 30,
-        marginRight: 30,
-        paddingLeft: 16
-    },
-    button: {
-        backgroundColor: '#788eec',
-        marginLeft: 30,
-        marginRight: 30,
-        marginTop: 20,
-        height: 48,
-        borderRadius: 5,
-        alignItems: "center",
-        justifyContent: 'center'
-    },
-    buttonTitle: {
-        color: 'white',
-        fontSize: 16,
-        fontWeight: "bold"
-    },
-    footerView: {
-        flex: 1,
-        alignItems: "center",
-        marginTop: 20
-    },
-    footerText: {
-        fontSize: 16,
-        color: '#2e2e2d'
-    },
-    footerLink: {
-        color: "#788eec",
-        fontWeight: "bold",
-        fontSize: 16
-    }
-})
+  // REG COIN INFO POP UP
+
+  eco_logo: {
+    width: 100,
+    height: 80,
+    marginTop: 30,
+  },
+
+  content_container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  },
+
+  welcome_text: {
+    fontSize: 30,
+  },
+
+  eco_about: {
+    fontSize: 20,
+    padding: 20,
+    lineHeight: 27,
+    letterSpacing: 1,
+  },
+
+  line_break: {
+    width: "70%",
+    height: 1,
+    backgroundColor: "#3f3f3f",
+    marginTop: 20,
+    marginBottom: 20,
+  },
+});
